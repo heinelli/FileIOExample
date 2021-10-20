9fea2d7a923a07b7fdb5e03fc9ca2a214dfc40ab
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-20 08:41:08 -0600

    Add project files.

---
+++ FileIOExample/FileIOForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class FileIOForm
+    Private Sub FileIOForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WriteFile()
+    End Sub
+
+    Sub WriteFile()
+        Dim fileName As String = "ExampleF21.txt"
+
+        FileOpen(1, fileName, OpenMode.Input)
+
+        FileClose(1)
+
+
+    End Sub
+End Class
